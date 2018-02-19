a3034f2c4e8f25cb8c6170c32e641d25c38f5905
trevoruptain <trevoru@uw.edu>
2018-02-19 09:32:48 -0800

    Fix conflicts

--- react-minesweeper/components/board.jsx
+++ react-minesweeper/components/board.jsx
@@ -18,7 +18,7 @@ class Board extends React.Component {
         <div>What is down???</div>
         <div>This is my favorite line</div>
         <div>This is my second favorite line</div>
-        <div>Last line for real this time</div>
+        <div>Last line for real this time. This line is fake.</div>
       </div>
     );
   }
